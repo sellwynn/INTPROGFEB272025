83151287e955817260c1cf34dbbee55ac91242ae
emnlvj <150634271+emnlvj@users.noreply.github.com>
2025-02-27 16:49:49 +0800

    Final

--- app/src/main/java/com/example/intprogfeb27/Profilepage.kt
+++ app/src/main/java/com/example/intprogfeb27/Profilepage.kt
@@ -3,15 +3,52 @@ package com.example.intprogfeb27
 import android.app.Activity
 import android.content.Intent
 import android.media.Image
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class Profilepage : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class Profilepage : AppCompatActivity() {
+
+        private lateinit var displayProfileImage: ImageView
+        private lateinit var displayName: TextView
+        private lateinit var displayEmail: TextView
+        private lateinit var displayNumber: TextView
+        private lateinit var displayStatus: TextView
+        private lateinit var displayLocation: TextView
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profilepage)
+
+            displayProfileImage = findViewById(R.id.profile_image)
+            displayName = findViewById(R.id.profileTextView)
+            displayStatus = findViewById(R.id.profile_role)
+            displayNumber = findViewById(R.id.profile_phone)
+            displayEmail = findViewById(R.id.profile_email)
+            displayLocation = findViewById(R.id.profile_location)
+
+            val name: String? = intent.getStringExtra("Name") ?:"Marshall D. Teach"
+            val email: String? = intent.getStringExtra("Email") ?:"blackbeardPirateKing@gmail.com"
+            val phone: String? = intent.getStringExtra("Phone") ?:"+63-929-555-4642"
+            val status: String? = intent.getStringExtra("Status") ?:"Commuter"
+            val location: String? = intent.getStringExtra("Location") ?: "No Specified Location"
+            val imageUriString = intent.getStringExtra("Image_Uri")
+            displayName.text = "Name: $name"
+            displayEmail.text = "Email: $email"
+            displayStatus.text = "$status"
+            displayNumber.text = "$phone"
+            displayLocation.text = "$location"
+
+            if(!imageUriString.isNullOrEmpty()){
+                val imageUri = Uri.parse(imageUriString)
+
+                displayProfileImage.setImageURI(imageUri)
+            }
 
         val homebutton: ImageView = findViewById(R.id.homebutton)
         homebutton.setOnClickListener {
