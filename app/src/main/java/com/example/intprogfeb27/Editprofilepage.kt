c017f18b6c9017dea4ec2ba34534befc3350364e
emnlvj <150634271+emnlvj@users.noreply.github.com>
2025-02-27 17:23:29 +0800

    Final Commit

--- app/src/main/java/com/example/intprogfeb27/Editprofilepage.kt
+++ app/src/main/java/com/example/intprogfeb27/Editprofilepage.kt
@@ -1,67 +1,101 @@
 package com.example.intprogfeb27
 
 import android.app.Activity
-import android.content.SharedPreferences
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class Editprofilepage : Activity() {
-    private lateinit var sharedPreferences: SharedPreferences
+class Editprofilepage : AppCompatActivity() {
+    private lateinit var profileImage: ImageView
+    private lateinit var nameInput: EditText
+    private lateinit var phoneEdit: EditText
+    private lateinit var statusEdit: EditText
+    private lateinit var emailInput: EditText
+    private lateinit var countryInput: EditText
+    private lateinit var provInput: EditText
+    private lateinit var saveButton: Button
+    private var selectedImageUri: Uri? = null // Temporary image storage
+
+
+    companion object {
+        private const val PICK_IMAGE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.editprofilepage)
 
-        sharedPreferences = getSharedPreferences("TempProfile", MODE_PRIVATE)
+        profileImage = findViewById(R.id.profileImage)
+        nameInput = findViewById(R.id.nameEdit)
+        statusEdit = findViewById(R.id.statusEditText)
+        emailInput = findViewById(R.id.emailEditText)
+        phoneEdit = findViewById(R.id.phoneEditText)
+        countryInput = findViewById(R.id.countryEditText)
+        provInput = findViewById(R.id.provinceEditText)
+        saveButton = findViewById(R.id.saveButton)
 
 
-        val backButton: ImageView = findViewById(R.id.backButton)
-        val editName: EditText = findViewById(R.id.editProfileText)
-        val editStatus: EditText = findViewById(R.id.statusEditText)
-        val editEmail: EditText = findViewById(R.id.emailEditText)
-        val editPhone: EditText = findViewById(R.id.phoneEditText)
-        val editCountry: EditText = findViewById(R.id.countryEditText)
-        val editProvince: EditText = findViewById(R.id.provinceEditText)
-        val saveButton: Button = findViewById(R.id.saveButton)
-        val cancelButton: Button = findViewById(R.id.cancelButton)
 
-
-        editName.setText(sharedPreferences.getString("profileName", "Marshall D. Teach"))
-        editStatus.setText(sharedPreferences.getString("profileStatus", "Commuter"))
-        editEmail.setText(sharedPreferences.getString("profileEmail", "blackbeardPirateKing@gmail.com"))
-        editPhone.setText(sharedPreferences.getString("profilePhone", "+63-929-555-4642"))
-        editCountry.setText(sharedPreferences.getString("profileCountry", "Philippines"))
-        editProvince.setText(sharedPreferences.getString("profileProvince", "Cebu"))
-
-
-        backButton.setOnClickListener {
-            finish()
+        profileImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, PICK_IMAGE)
         }
-
 
         saveButton.setOnClickListener {
-            sharedPreferences.edit()
-                .putString("profileName", editName.text.toString())
-                .putString("profileStatus", editStatus.text.toString())
-                .putString("profileEmail", editEmail.text.toString())
-                .putString("profilePhone", editPhone.text.toString())
-                .putString("profileCountry", editCountry.text.toString())
-                .putString("profileProvince", editProvince.text.toString())
-                .apply()
+            val tempName = nameInput.text.toString()
+            val tempEmail = emailInput.text.toString()
+            val tempPhone = phoneEdit.text.toString()
+            val tempProv = provInput.text.toString()
+            val tempCountry = countryInput.text.toString()
+            val tempStatus = statusEdit.text.toString()
 
-            finish()
+            val location = "$tempProv, $tempCountry"
+            val intent = Intent(this, Profilepage::class.java).apply {
+                putExtra("Name", tempName)
+                putExtra("Email", tempEmail)
+                putExtra("Phone", tempPhone)
+                putExtra("Location", location)
+                putExtra("Status", tempStatus)
+                putExtra("Image_Uri", selectedImageUri?.toString())
+            }
+            startActivity(intent)
         }
 
 
+        val backButton: ImageView= findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            val intent = Intent(this, LandingPage::class.java)
+            startActivity(intent)
+            finish()
+        }
+        val cancelButton: Button = findViewById(R.id.cancelButton)
         cancelButton.setOnClickListener {
-            editName.setText(sharedPreferences.getString("profileName", "Marshall D. Teach"))
-            editStatus.setText(sharedPreferences.getString("profileStatus", "Commuter"))
-            editEmail.setText(sharedPreferences.getString("profileEmail", "blackbeardPirateKing@gmail.com"))
-            editPhone.setText(sharedPreferences.getString("profilePhone", "+63-929-555-4642"))
-            editCountry.setText(sharedPreferences.getString("profileCountry", "Philippines"))
-            editProvince.setText(sharedPreferences.getString("profileProvince", "Cebu"))
+            val intent = Intent(this, Profilepage::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK && data != null)
+        {
+
+            selectedImageUri = data.data
+            profileImage.setImageURI(selectedImageUri)
         }
     }
 }
--- app/src/main/java/com/example/intprogfeb27/Profilepage.kt
+++ app/src/main/java/com/example/intprogfeb27/Profilepage.kt
@@ -2,63 +2,86 @@ package com.example.intprogfeb27
 
 import android.app.Activity
 import android.content.Intent
-import android.content.SharedPreferences
+import android.media.Image
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class Profilepage : Activity() {
-    private lateinit var sharedPreferences: SharedPreferences
+class Profilepage : AppCompatActivity() {
+
+    private lateinit var displayProfileImage: ImageView
+    private lateinit var displayName: TextView
+    private lateinit var displayEmail: TextView
+    private lateinit var displayNumber: TextView
+    private lateinit var displayStatus: TextView
+    private lateinit var displayLocation: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profilepage)
 
-        sharedPreferences = getSharedPreferences("TempProfile", MODE_PRIVATE)
+        displayProfileImage = findViewById(R.id.profile_image)
+        displayName = findViewById(R.id.profileTextView)
+        displayStatus = findViewById(R.id.profile_role)
+        displayNumber = findViewById(R.id.profile_phone)
+        displayEmail = findViewById(R.id.profile_email)
+        displayLocation = findViewById(R.id.profile_location)
+
+        val name: String? = intent.getStringExtra("Name") ?:"Marshall D. Teach"
+        val email: String? = intent.getStringExtra("Email") ?:"blackbeardPirateKing@gmail.com"
+        val phone: String? = intent.getStringExtra("Phone") ?:"+63-929-555-4642"
+        val status: String? = intent.getStringExtra("Status") ?:"Commuter"
+        val location: String? = intent.getStringExtra("Location") ?: "Cebu, Philippines"
+        val imageUriString = intent.getStringExtra("Image_Uri")
+        displayName.text = "Name: $name"
+        displayEmail.text = "Email: $email"
+        displayStatus.text = "$status"
+        displayNumber.text = "$phone"
+        displayLocation.text = "$location"
+
+        if(!imageUriString.isNullOrEmpty()){
+            val imageUri = Uri.parse(imageUriString)
+
+            displayProfileImage.setImageURI(imageUri)
+        }
 
         val homebutton: ImageView = findViewById(R.id.homebutton)
         homebutton.setOnClickListener {
-            startActivity(Intent(this, LandingPage::class.java))
+            // Navigate to Landing Page
+            val intent = Intent(this, LandingPage::class.java)
+            startActivity(intent)
+            finish() //
         }
 
         val devbutton: ImageView = findViewById(R.id.devbutton)
         devbutton.setOnClickListener {
-            startActivity(Intent(this, Developerpage::class.java))
+            // Navigate to Landing Page
+            val intent = Intent(this, Developerpage::class.java)
+            startActivity(intent)
+            finish() //
         }
 
         val settingbutton: ImageView = findViewById(R.id.settingbutton)
         settingbutton.setOnClickListener {
-            startActivity(Intent(this, SettingsPage::class.java))
+            // Navigate to Landing Page
+            val intent = Intent(this, SettingsPage::class.java)
+            startActivity(intent)
+            finish() //
         }
 
         val buttony: Button = findViewById(R.id.buttony)
         buttony.setOnClickListener {
-            startActivity(Intent(this, Editprofilepage::class.java))
+            // Navigate to Landing Page
+            val intent = Intent(this, Editprofilepage::class.java)
+            startActivity(intent)
+            finish() //
         }
 
-        loadProfileData()
-    }
 
-    override fun onResume() {
-        super.onResume()
-        loadProfileData()
-    }
-
-    private fun loadProfileData() {
-        val profileTextView: TextView = findViewById(R.id.profileTextView)
-        val profileRole: TextView = findViewById(R.id.profile_role)
-        val profileEmail: TextView = findViewById(R.id.profile_email)
-        val profilePhone: TextView = findViewById(R.id.profile_phone)
-        val profileLocation: TextView = findViewById(R.id.profile_location)
-
-        profileTextView.text = sharedPreferences.getString("profileName", "Marshall D. Teach")
-        profileRole.text = sharedPreferences.getString("profileStatus", "Commuter")
-        profileEmail.text = sharedPreferences.getString("profileEmail", "blackbeardPirateKing@gmail.com")
-        profilePhone.text = sharedPreferences.getString("profilePhone", "+63-929-555-4642")
-
-        val country = sharedPreferences.getString("profileCountry", "Philippines")
-        val province = sharedPreferences.getString("profileProvince", "Cebu")
-        profileLocation.text = "$province, $country"
     }
 }
